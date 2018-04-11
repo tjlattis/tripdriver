5af699698e752f26c506419a0e4099ec94b3f9ea
Anthony Lattis <tjlattis@gmail.com>
2018-04-11 11:14:16 -0500

    Change driver to make it work yay!

--- webdriver.py
+++ webdriver.py
@@ -8,6 +8,9 @@ from selenium.webdriver.common.by import By
 import bs4
 import traceback
 
+import sys
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
 driver = webdriver.Firefox()
 driver.get('https://www.tripadvisor.com/Attraction_Review-g294201-d553185-Reviews-Keops_Pyramid-Cairo_Cairo_Governorate.html')
 source = driver.page_source
@@ -21,14 +24,19 @@ try:
     
     for page in range(int(last[0].text) - 1):
 
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "review-container")))
+        # <div class="loadingWhiteBox">
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "next")))
+        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "next")))
         
+##        reviews = driver.find_elements_by_class_name('review-container')
+##        for review in reviews:
+##            pages.append(review.text)
 
-        reviews = driver.find_elements_by_class_name('review-container')
-        for review in reviews:
-            pages.append(review.text)
-
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "next")))
+        page_obj = bs4.BeautifulSoup(driver.page_source, "html.parser")
+        page = page_obj.select("#taplc_location_reviews_list_responsive_detail_0")
+        pages.append(page[0])
+        # print(page[0].text.translate(non_bmp_map))
+        
         driver.find_element_by_class_name('next').click()
         
 except Exception as ex:
